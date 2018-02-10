bc225d0f4416a888070f6f5c6d259a962ebdd0c0
sollent <pavel.laikov98@gmail.com>
2018-02-10 14:14:30 +0300

    modification the front-end invironment 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'), // Подключили пакет Gulp
+		sass = require('gulp-sass'), // Подключили sass пакет
+		browserSync = require('browser-sync'); // Подключили Browser Sync
 
-gulp.task('my_task', function(){
-	console.log('Hello my little girl, I am with you again!');
+gulp.task('sass', function(){
+	return gulp.src('app/sass/style.sass') // Берем источник sass файла
+		.pipe(sass()) // Преобразуем sass в css посредством gulp-sass
+		.pipe(gulp.dest('app/css')) // Выгружаем результат в папку app/css
+		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при зменении
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({ // Выполняем browser Sync
+		server: { // Определяем параметры сервера
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function(){
+	gulp.watch('app/sass/**/*.sass', ['sass']) // Наблюдение за sass файлами
 });
